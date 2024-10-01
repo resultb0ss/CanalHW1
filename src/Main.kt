0c3aa6a93b915e03684803064aa4f94c68361095
Admin <reteamboss@gmail.com>
2024-10-01 11:13:47 +0300

    1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,36 @@
-fun main() {
-    println("Hello World!")
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
+import kotlin.system.measureTimeMillis
+import kotlin.time.measureTime
+
+suspend fun main() {
+    val text = Storage().text
+    val str = text.split("!", ",", ".", ":", ";").map { it.split(" ").toList() }
+    val channel = Channel<String>()
+    val time = measureTimeMillis {
+        coroutineScope {
+            launch {
+                str.getList(channel)
+                }
+
+            launch {
+                for (i in channel){
+                println("Получены даные $i") }
+            }
+        }
+    }
+    println("Время затраченное на выполнение: $time")
+
+
+
+}
+
+suspend infix fun List<List<String>>.getList(channel: SendChannel<String>) {
+    for (i in this.indices) {
+        for (j in this[i]) {
+            delay(100L)
+            channel.send(j)
+        }
+    }
+    channel.close()
 }
